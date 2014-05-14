5f364179ff4e5b771f951e55e16b9a89fe8167d0
wyhw <wyhw@msn.com>
2014-05-14 17:24:44 +0800

    elasticsearch plugin : custom status

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-elasticsearch-custom-status
+Elasticsearch custom status
 ===========================
 
-Can set custom status on elasticsearch
+A elasticsearch plugin, Control elasticsearch custom status.
+
+##Instalation
+Run 
+
+	cd elasticsearch
+
+	wget http://github.com/wyhw/elasticsearch-custom-status/blob/master/elasticsearch-custom-status-0.0.1.zip?raw=true --no-check-certificate
+
+	bin/plugin --url file://`pwd`/elasticsearch-custom-status-0.0.1.zip --install elasticsearch-custom-status
+
+then Restart elasticsearch.
+
+##Usage
+
+####View status: 
+
+	http://localhost:9200/_customstatus
+
+####Set custom status
+
+	http://localhost:9200/_customstatus?status=meetyou
+
+
+##Version mapping
+
+	master   =>    Elasticsearch 1.0.x
+
+	0.0.1    =>    Elasticsearch 1.0.x
+
+
---
+++ src/main/java/org/elasticsearch/plugin/bx/rest/action/CustomStatusAction.java
@@ -0,0 +1,81 @@
+/*
+   Copyright 2012 Wyhw
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package org.elasticsearch.plugin.bx.rest.action;
+
+import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestStatus.OK;
+
+import java.io.IOException;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.indices.IndicesService;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.RestChannel;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.XContentRestResponse;
+import org.elasticsearch.rest.XContentThrowableRestResponse;
+import org.elasticsearch.rest.action.support.RestXContentBuilder;
+
+public class CustomStatusAction extends BaseRestHandler {
+	private static String status = "";
+    @Inject
+    public CustomStatusAction(Settings settings, Client client,
+            RestController controller, IndicesService indicesService) {
+        super(settings, client);
+        controller.registerHandler(GET, "/_customstatus", this);
+    }
+
+
+    @Override
+    public void handleRequest(final RestRequest request,
+            final RestChannel channel) {
+
+        String _status = request.param("status", null);
+
+        try {
+            // Wait for trigger
+            if (_status != null) {
+            	status = _status;
+            }
+            
+            XContentBuilder builder = createXContentBuilderForReload(request);
+            channel.sendResponse(new XContentRestResponse(request, OK, builder));
+        } catch (Exception e) {
+            try {
+                channel.sendResponse(new XContentThrowableRestResponse(request, e));
+            } catch (IOException e1) {
+            }
+        }
+    }
+
+    private XContentBuilder createXContentBuilderForReload(final RestRequest request) {
+        XContentBuilder builder = null;
+        try {
+            builder = RestXContentBuilder.restContentBuilder(request);
+            builder.startObject();
+            builder.field("ok", "true");
+            builder.field("status", status.length() == 0 ? "true" : status);
+            builder.endObject();
+        } catch (Exception e) {
+        }
+        return builder;
+    }
+}
